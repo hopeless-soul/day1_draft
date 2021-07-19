c445fefbf980413378c14b5a0160e02b6d5ade57
hopeless-soul <57869028+hopeless-soul@users.noreply.github.com>
2021-07-19 18:04:18 +0300

    Update test.js
    pl() function fix bag in range of (11-19).

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-function sumEven(endNum) {
+function sum(startNum, endNum) {
     let sum = 0; 
-    for(let i=0; i<endNum; i++)
+    for(let i=Math.min(startNum, endNum); i<Math.max(startNum, endNum); i++)
         sum += i;
     return sum;
 }
-// console.log(sumEven(10).toString(), " >> '45'" );
+// console.log(sum(15, 10));
 
 function doFactorial(endNum) {
     let result = 1; 
@@ -12,12 +12,14 @@ function doFactorial(endNum) {
         result *= i;
     return result;
 }
-// console.log(doFactorial(5), " >> '120'");
+// console.log(doFactorial(5));
 
 function pl(count, one, few, many) {
-    if(count % 10 == 1){ return `${count} ${one}`; }
+    if(count > 10 && count < 20 ){ return `${count} ${many}`; }
+    else if(count % 10 == 1){ return `${count} ${one}`; }
     else if( count % 10 >= 2 && count % 10 <= 4 ){ return `${count} ${few}`; }
     else if( count % 10 >= 5 && count % 10 < 10 || count % 10 == 0){ return `${count} ${many}`; }
     return;
 }
-console.log(pl(10359, 'стул', 'стула', 'стульев'));
+console.log(pl(0, 'стул', 'стула', 'стульев'));
+
